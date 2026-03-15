cc23665b4d7f53fed89724612d2dc1e335e57a0b
Eggroley <github.egg@kaerimiru.me>
2026-03-15 00:29:02 -0500

    fix tor path in electron-cash script

--- flatpak/electron-cash.sh
+++ flatpak/electron-cash.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-PY_VER=$(python3 -c 'import sys; print(f"python{sys.version_info.major}.{sys.version_info.minor}")')
-export LD_LIBRARY_PATH=/app/lib:/app/lib/${PY_VER}/site-packages/electroncash/tor/bin:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=/app/lib:/app/lib/electroncash/tor/bin:${LD_LIBRARY_PATH}
 exec /app/bin/electron-cash.real "$@"
